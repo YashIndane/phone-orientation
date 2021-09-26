5e3ffe6d8e1803439af30fa02b361bfb10e2e65a
Yash Indane <yashindane46@gmail.com>
2021-09-26 16:52:35 +0530

    first commit

---
+++ model_training.py
@@ -0,0 +1,41 @@
+"""
+This script is for training the model
+"""
+
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+
+dataset = pd.read_csv("rotation_vector_readings.csv", header = None)
+
+# Reading values
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, -1].values
+
+# Building model and training
+model = Sequential()
+
+# Adding first layer with 5 neurons
+model.add(Dense(units = 5, activation = "relu", input_dim = 3))
+
+# Adding second layer with 4 neurons
+model.add(Dense(units = 4, activation = "relu"))
+
+# Adding third layer with 3 neurons
+model.add(Dense(units = 3, activation = "relu"))
+
+# Adding output layer
+model.add(Dense(units = 1, activation = "sigmoid"))
+
+# Compiling model
+model.compile(loss = "binary_crossentropy",
+            optimizer = Adam(
+                learning_rate = 0.01
+            )
+)
+
+# Training the model
+model.fit(X, Y, epochs = 100)
+
+model.save("model-3.h5")
